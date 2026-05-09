1ded960bfe3b838e3f27f902d9439aa2172863bd
Parth kukadiya <parthkukadiya636@gmail.com>
2026-05-09 23:07:10 +0530

    CPP

---
+++ Student_Management_sys.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class Student
+{
+public:
+    int rollNo;
+    string name;
+    int age;
+    string course;
+
+    void addStudent(){
+        ofstream file("student.txt", ios::app);
+
+        cout<<"\nEnter the roll number";
+        cin >> rollNo;
+
+        cin.ignore();
+
+        cout << "Enter Name: ";
+        getline(cin, name);
+
+        cout << "Enter Age: ";
+        cin >> age;
+
+        cin.ignore();
+
+        cout << "Enter Course: ";
+        getline(cin, course);
+
+        file << rollNo << "|" << name << "|" << age << "|" << course << endl;
+
+        file.close();
+
+        cout << "\nStudent Record Added Successfully!\n";
+    }
+
+    void displayStudents(){
+        ifstream file("student.txt"); // file open
+
+        string data;
+
+        while(getline(file,data)){
+            cout<<data<<endl;
+        }
+
+        file.close();
+}
+};
+
+
+int main(){
+
+    Student s;
+    int choice;
+
+    do{
+        cout << "\n===== Student Management System =====\n";
+        cout << "1. Add Student\n";
+        cout << "2. Display Students\n";
+        cout << "3. Search Student\n";
+        cout << "4. Delete Student\n";
+        cout << "5. Exit\n";
+
+        cout << "\nEnter Your Choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            s.addStudent();
+            break;
+
+        case 2:
+            s.displayStudents();
+            break;
+
+        // case 3:
+        //     s.searchStudent();
+        //     break;
+
+        // case 4:
+        //     s.deleteStudent();
+        //     break;
+
+        // case 5:
+        //     cout << "\nExiting Program...\n";
+        //     break;
+
+        // default:
+        //     cout << "\nInvalid Choice!\n";
+        }
+
+    } while (choice != 5);
+
+    return 0;
+
+}
